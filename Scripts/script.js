011ab61ad8991be2d77481df75bea205e9b67acd
Karan Kushwaha <110368549+iamkarankushwaha@users.noreply.github.com>
2025-12-03 00:35:41 +0530

    Created new website.

---
+++ Scripts/script.js
@@ -0,0 +1,23 @@
+// Header Switches
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('header');
+    if (window.scrollY > 80) {
+        header.classList.add('scrolled');
+    }
+    else {
+        header.classList.remove('scrolled');
+    }
+})
+
+
+// menu works
+function openMenu() {
+    const navcontainer = document.getElementById('navcontainer');
+    navcontainer.classList.add('openMenu');
+}
+function closeMenu() {
+    const navcontainer = document.getElementById('navcontainer');
+    navcontainer.classList.remove('openMenu');
+}
+const navcontainer = document.getElementById('navcontainer');
+navcontainer.addEventListener('click', closeMenu);
